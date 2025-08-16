79f3f93c54749743c0c57bbd2164d356f8a742d8
Shafny Hadhy <mohamedshafny02@gmail.com>
2025-08-16 20:38:30 +0530

    added a bodyContent for testing

---
+++ src/pages/bodyContent.jsx
@@ -0,0 +1,9 @@
+export default function BodyContent(){
+    return(
+        <div className="w-[500px] h-[500px] flex justify-center items-center bg-accent m-[50px]">
+            <button className="w-[100px] h-[50px] bg-white rounded-2xl">
+                Log In
+            </button>
+        </div>
+    )
+}
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "../components/header";
+import BodyContent from "./bodyContent";
 
 export default function HomePage(){
     return(
         <div className="w-full h-full bg-primary">
            <Header/>
+           <BodyContent/>
            <Routes path="/">
                 <Route path="/" element={<h1>Welcome to Home Page</h1>}/>
                 <Route path="/product" element={<h1>Product list</h1>}/>
