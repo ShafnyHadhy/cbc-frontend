0764d0c60446176123f8f7f2625cdb702a2b1252
Shafny Hadhy <mohamedshafny02@gmail.com>
2025-08-16 20:44:33 +0530

    logOut button added

--- src/pages/bodyContent.jsx
+++ src/pages/bodyContent.jsx
@@ -1,8 +1,11 @@
 export default function BodyContent(){
     return(
-        <div className="w-[500px] h-[500px] flex justify-center items-center bg-accent m-[50px]">
+        <div className="w-[500px] h-[500px] flex justify-center items-center bg-accent m-[50px] gap-[20px]">
             <button className="w-[100px] h-[50px] bg-white rounded-2xl">
-                Log In
+                Sign up
+            </button>
+            <button className="w-[100px] h-[50px] bg-white rounded-2xl">
+                Log out
             </button>
         </div>
     )
